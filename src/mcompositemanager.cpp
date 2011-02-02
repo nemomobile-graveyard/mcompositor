nemomobile-graveyard/mcompositor

commit 4a7761a226315877686de303d70fd4ba91f43863

Fixes: NB#223544 - Often Lock Screen UI is transparent - make sure window does not have old transformations when it is mapped again

src/mcompositemanager.cpp
@@ -2370,6 +2370,9 @@ void MCompositeManagerPrivate::mapEvent(XMapEvent *e)
         enableCompositing(true);
 
     if (item && pc) {
+        // reset item for the case previous animation did not end cleanly
+        item->setUntransformed();
+        item->setPos(0, 0);
         if (wtype == MCompAtoms::NORMAL || pc->windowTypeAtoms().isEmpty())
             pc->setWindowTypeAtom(ATOM(_NET_WM_WINDOW_TYPE_NORMAL));
         else

commit 473ad85848c8169421d5217a1ffa9fcfb821680d

fix regression introduced by 4a7761a226315877686de303d70fd4ba91f43863 - respect the window position instead of assuming (0, 0)

src/mcompositemanager.cpp
@@ -2372,7 +2372,7 @@ void MCompositeManagerPrivate::mapEvent(XMapEvent *e)
     if (item && pc) {
         // reset item for the case previous animation did not end cleanly
         item->setUntransformed();
-        item->setPos(0, 0);
+        item->setPos(pc->realGeometry().x(), pc->realGeometry().y());
         if (wtype == MCompAtoms::NORMAL || pc->windowTypeAtoms().isEmpty())
             pc->setWindowTypeAtom(ATOM(_NET_WM_WINDOW_TYPE_NORMAL));
         else