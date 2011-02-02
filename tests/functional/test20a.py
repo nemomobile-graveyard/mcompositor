a6fe677df4417c9da32bf9760fe8fe2bfedcc8b9
Adam Endrodi <ext-adam.endrodi@nokia.com>
2011-02-02 18:19:55 +0200

    split test20
    into test20a (jammed app) and test20b (phone call)

---
+++ tests/functional/test20a.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+# Check that jammed apps are decorated.
+
+#* Test steps
+#  * show an application window that does not respond to pings
+#  * wait for 7 seconds to make compositor notice it jammed
+#* Post-conditions
+#  * the decorator is on top of it
+
+import os, re, sys, time
+
+if os.system('mcompositor-test-init.py'):
+  sys.exit(1)
+
+fd = os.popen('windowstack m')
+s = fd.read(5000)
+win_re = re.compile('^0x[0-9a-f]+')
+deco_win = 0
+for l in s.splitlines():
+  if re.search(' viewable mdecorator', l.strip()):
+    deco_win = win_re.match(l.strip()).group()
+
+if deco_win == 0:
+  print 'FAIL: decorator window not found'
+  sys.exit(1)
+
+# create application window that does not respond to pings
+fd = os.popen('windowctl pkn')
+app_win = fd.readline().strip()
+time.sleep(7)
+
+ret = 0
+fd = os.popen('windowstack m')
+s = fd.read(5000)
+for l in s.splitlines():
+  if re.search("%s " % deco_win, l.strip()):
+    print deco_win, 'found'
+    break
+  elif re.search("%s " % app_win, l.strip()):
+    print 'FAIL: decorator is below the application'
+    print 'Failed stack:\n', s
+    ret = 1
+    break
+
+if os.system('/usr/bin/gconftool-2 --type bool --set /desktop/meego/notifications/previews_enabled true'):
+  print 'cannot re-enable notifications'
+
+sys.exit(ret)
--- tests/functional/test20b.py
+++ tests/functional/test20b.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 
-# Check that jammed and (during call) fullscreen apps are decorated.
+# Check that fullscreen apps during callare decorated.
 
 #* Test steps
-#  * show an application window that does not respond to pings
-#  * wait for 7 seconds to make compositor notice it jammed
-#  * check that the decorator is on top of it
-#  * close it
 #  * simulate an ongoing call
 #  * show a fullscreen application window
 #* Post-conditions
-#  * check that the decorator is on top of it
+#  * the decorator is on top of it
 
 import os, re, sys, time
 
@@ -28,28 +24,6 @@ for l in s.splitlines():
 if deco_win == 0:
   print 'FAIL: decorator window not found'
   sys.exit(1)
-
-# create application window that does not respond to pings
-fd = os.popen('windowctl pkn')
-app_win = fd.readline().strip()
-time.sleep(7)
-
-ret = 0
-fd = os.popen('windowstack m')
-s = fd.read(5000)
-for l in s.splitlines():
-  if re.search("%s " % deco_win, l.strip()):
-    print deco_win, 'found'
-    break
-  elif re.search("%s " % app_win, l.strip()):
-    print 'FAIL: decorator is below the application'
-    print 'Failed stack:\n', s
-    ret = 1
-    break
-
-# close it
-os.popen('pkill windowctl')
-time.sleep(1)
 
 # simulate a phone call
 fd = os.popen('windowctl P')
@@ -75,7 +49,6 @@ for l in s.splitlines():
 # cleanup
 os.popen('pkill windowctl')
 os.popen('pkill context-provide')
-time.sleep(1)
 
 if os.system('/usr/bin/gconftool-2 --type bool --set /desktop/meego/notifications/previews_enabled true'):
   print 'cannot re-enable notifications'
