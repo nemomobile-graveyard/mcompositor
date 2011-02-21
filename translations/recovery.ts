510e6880086255a99ffea431955045d95557a9b8
Adam Endrodi <ext-adam.endrodi@nokia.com>
2011-02-21 19:15:59 +0200

    don't try to GetWMName(None)
    ...like when the decorator is told to unmanage its window.
    Avoids an X roundtrip.

--- translations/recovery.ts
+++ translations/recovery.ts
@@ -4,22 +4,22 @@
 <context>
     <name></name>
     <message id="qtn_reco_app_not_responding">
-        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="455"/>
+        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="454"/>
         <source></source>
         <translation>%1 sucks</translation>
     </message>
     <message id="qtn_reco_close_app_question">
-        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="462"/>
+        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="461"/>
         <source></source>
         <translation>Do you want to close it?</translation>
     </message>
     <message id="qtn_comm_command_yes">
-        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="470"/>
+        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="469"/>
         <source></source>
         <translation>Aje</translation>
     </message>
     <message id="qtn_comm_command_no">
-        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="472"/>
+        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="471"/>
         <source></source>
         <translation>Oh, no!</translation>
     </message>
