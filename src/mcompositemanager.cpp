b9ecae28b805669e8a913b83c4b0dacda89fb042
Adam Endrodi <ext-adam.endrodi@nokia.com>
2011-03-16 15:27:54 +0200

    redirect desktop when a window is topped
    We already do it when the window is raised.
    
      * src/mcompositemanager.cpp (MCompositeManagerPrivate::rootMessageEvent):
        Now do it also when a window is activated.  Failing that, in the background
        can be junk.

--- src/mcompositemanager.cpp
+++ src/mcompositemanager.cpp
@@ -2248,8 +2248,17 @@ void MCompositeManagerPrivate::rootMessageEvent(XClientMessageEvent *event)
                 if (needComp)
                     enableCompositing();
             }
-        } else if (event->window != stack[DESKTOP_LAYER])
+        } else if (event->window != stack[DESKTOP_LAYER]) {
+            // unless we redirect the desktop we run the risk of using trash
+            // in the animation because nothing is drawn there and the buffer
+            // contents is undefined
+            MCompositeWindow *d = COMPOSITE_WINDOW(stack[DESKTOP_LAYER]);
+            if (d && ((MTexturePixmapItem *)d)->isDirectRendered()) {
+                ((MTexturePixmapItem *)d)->enableRedirectedRendering();
+                setWindowDebugProperties(d->window());
+            }
             setExposeDesktop(false);
+        }
 
 #ifdef ENABLE_BROKEN_SIMPLEWINDOWFRAME
         FrameData fd = framed_windows.value(event->window);
