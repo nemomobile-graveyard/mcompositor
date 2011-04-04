99ccdc238bbf3bb794362c944fe65b6f46dd3c45
Kimmo Hämäläinen <kimmo.hamalainen@nokia.com>
2011-04-04 14:57:23 +0300

    fix a drawing glitch when display dims - input-only event eater obscured our windows - avoid calling displayOff when display just dims

--- src/mcompositemanager.cpp
+++ src/mcompositemanager.cpp
@@ -1770,6 +1770,7 @@ void MCompositeManagerPrivate::sendSyntheticVisibilityEventsForOurBabies()
          if (cw && cw->isMapped())
              pc = cw->propertyCache();
          if (cw && cw->isMapped() && (!pc->hasAlpha() || pc->opaqueWindow()) &&
+             !pc->isInputOnly() &&
              !pc->isDecorator() && !cw->hasTransitioningWindow() &&
              // allow input windows to composite their app, see NB#223280
              pc->windowTypeAtom() != ATOM(_NET_WM_WINDOW_TYPE_INPUT) &&
