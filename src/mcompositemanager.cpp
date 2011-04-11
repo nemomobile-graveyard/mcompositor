5ff86f448d2d06bc0c68e02f8f720ce81d6898d4
Jarosław Jaryszew <jaroslaw.jaryszew@teleca.com>
2011-04-11 15:02:10 +0200

    Fixes: NB#243142 RevBy: Details: MCompositor will now set additional context property containing desktop window orientation angle. It will be called "/Screen/Desktop/OrientationAngle" Class MCurrentWindowOrientationProvider was renamed to more generic name - MContextProviderWrapper.

--- src/mcompositemanager.cpp
+++ src/mcompositemanager.cpp
@@ -27,7 +27,7 @@
 #include "mdevicestate.h"
 #include "mcompositemanagerextension.h"
 #include "mcompmgrextensionfactory.h"
-#include "mcurrentwindoworientationprovider.h"
+#include "mcontextproviderwrapper.h"
 #include "mcompositordebug.h"
 #include "msplashscreen.h"
 #include "mcompositewindowanimation.h"
@@ -557,8 +557,8 @@ MCompositeManagerPrivate::MCompositeManagerPrivate(QObject *p)
       glwidget(0),
       compositing(true),
       changed_properties(false),
-      orientationProvider(qobject_cast<MCompositeManager*>(p)->
-                          configInt("default-current-window-angle")),
+      orientationProvider(qobject_cast<MCompositeManager*>(p)->configInt("default-current-window-angle"),
+                          qobject_cast<MCompositeManager*>(p)->configInt("default-desktop-angle")),
       prepared(false),
       stacking_timeout_check_visibility(false),
       stacking_timeout_timestamp(CurrentTime),
@@ -921,9 +921,12 @@ void MCompositeManagerPrivate::propertyEvent(XPropertyEvent *e)
         e->atom == ATOM(_MEEGOTOUCH_VIDEO_ALPHA)))
         dirtyStacking(true, e->time);
 
-    if (pc->isMapped() && e->window == current_app
-        && e->atom == ATOM(_MEEGOTOUCH_ORIENTATION_ANGLE))
-        orientationProvider.update(pc);
+    if (pc->isMapped()&& e->atom == ATOM(_MEEGOTOUCH_ORIENTATION_ANGLE)) {
+        if(e->window == stack[DESKTOP_LAYER])
+            orientationProvider.updateDesktopOrientationAngle(pc);
+        if(e->window == current_app)
+            orientationProvider.updateCurrentWindowOrienationAngle(pc);
+    }
 }
 
 Window MCompositeManagerPrivate::getLastVisibleParent(MWindowPropertyCache *pc)
@@ -1151,6 +1154,11 @@ bool MCompositeManagerPrivate::possiblyUnredirectTopmostWindow()
 
 void MCompositeManagerPrivate::unmapEvent(XUnmapEvent *e)
 {
+    //if desktop window was unmapped we need apropriate context property to be set to default
+    if (e->window == stack[DESKTOP_LAYER]) {
+        orientationProvider.updateDesktopOrientationAngle(0);
+        }
+
     if (e->send_event == True || e->event != QX11Info::appRootWindow())
         // handle root's SubstructureNotifys (top-levels) only
         return;
@@ -1806,7 +1814,7 @@ void MCompositeManagerPrivate::setCurrentApp(MCompositeWindow *cw,
     if (!cw)
         cw = COMPOSITE_WINDOW(w);
     // tell orientation provider that there is no current window by passing 0
-    orientationProvider.update(cw ? cw->propertyCache() : 0);
+    orientationProvider.updateCurrentWindowOrienationAngle(cw ? cw->propertyCache() : 0);
 
     prev = w;
 }
@@ -3500,6 +3508,7 @@ MCompositeWindow *MCompositeManagerPrivate::bindWindow(Window window)
     } else if (pc->windowType() == MCompAtoms::DESKTOP) {
         // just in case startup sequence changes
         stack[DESKTOP_LAYER] = window;
+        orientationProvider.updateDesktopOrientationAngle(getPropertyCache(window));
         dirtyStacking(false);
         return item;
     }
@@ -4391,4 +4400,5 @@ void MCompositeManager::ensureSettingsFile()
     config("default-current-window-angle",      270);
     config("lockscreen-map-timeout-ms",        1000);
     config("default-statusbar-height",           36);
+    config("default-desktop-angle",             270);
 }
---
+++ src/mcontextproviderwrapper.cpp
@@ -0,0 +1,49 @@
+#include "mcontextproviderwrapper_p.h"
+#include "mcontextproviderwrapper.h"
+
+#include "mwindowpropertycache.h"
+
+#include <contextprovider/ContextProvider>
+#include <QDBusConnection>
+
+MContextProviderWrapperPrivate::MContextProviderWrapperPrivate() :
+    service(QDBusConnection::SessionBus, "org.maemo.mcompositor.context"),
+    currentWindowAngleProperty(service, "/Screen/CurrentWindow/OrientationAngle"),
+    desktopAngleProperty(service, "/Screen/Desktop/OrientationAngle")
+{
+    currentWindowAngleProperty.setValue(defaultCurrentWindowAngle);
+    desktopAngleProperty.setValue(defaultDesktopAngle);
+}
+
+MContextProviderWrapper::~MContextProviderWrapper()
+{
+    // keep QScopedPointer happy
+}
+
+MContextProviderWrapper::MContextProviderWrapper(unsigned defaultCurrentWindowAngle,
+                                                 unsigned defaultDesktopAngle):
+    d_ptr(new MContextProviderWrapperPrivate)
+{
+    Q_D(MContextProviderWrapper);
+    d->q_ptr = this;
+    d->defaultCurrentWindowAngle = defaultCurrentWindowAngle;
+    d->defaultDesktopAngle = defaultDesktopAngle;
+}
+
+void MContextProviderWrapper::updateCurrentWindowOrienationAngle(MWindowPropertyCache *pc)
+{
+    Q_D(MContextProviderWrapper);
+    if(pc != 0)
+        d->currentWindowAngleProperty.setValue(pc->orientationAngle());
+    else
+        d->currentWindowAngleProperty.setValue(d->defaultCurrentWindowAngle);
+}
+
+void MContextProviderWrapper::updateDesktopOrientationAngle(MWindowPropertyCache *pc)
+{
+    Q_D(MContextProviderWrapper);
+    if(pc != 0)
+        d->desktopAngleProperty.setValue(pc->orientationAngle());
+    else
+        d->desktopAngleProperty.setValue(d->defaultDesktopAngle);
+}
