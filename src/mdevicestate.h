nemomobile-graveyard/mcompositor

commit 89fdfbbcfa42be01a785334803b09fdcc6f03cc0

Fixes: NB#253799 - Display slow to turn on when pressed powerkey while in call - monitor the touch screen lock mode, and don't wait for the touch screen lock   to paint if we are in unlocked state

src/mdevicestate.h
@@ -42,6 +42,7 @@ public:
     virtual bool displayOff() const { return display_off; }
     bool ongoingCall() const { return ongoing_call; }
     const QString &screenTopEdge() const { return screen_topedge; }
+    const QString &touchScreenLock() const { return touchScreenLockMode; }
 
 signals:
 
@@ -53,6 +54,8 @@ private slots:
 
 #ifdef GLES2_VERSION
     void mceDisplayStatusIndSignal(QString mode);
+    void mceTouchScreenLockSignal(QString mode);
+    void gotTouchScreenLockMode(QDBusPendingCallWatcher *watcher);
 #endif
     void callPropChanged();
     void topPropChanged();
@@ -61,11 +64,13 @@ private:
 
 #ifdef GLES2_VERSION
     QDBusConnection *systembus_conn;
+    QDBusPendingCallWatcher *tsmode_call;
 #endif
     ContextProperty *call_prop, *top_prop;
     bool display_off;
     bool ongoing_call;
     QString screen_topedge;
+    QString touchScreenLockMode;
 };
 
 #endif

src/mcompositemanager.cpp
@@ -2770,7 +2770,8 @@ void MCompositeManagerPrivate::displayOff(bool display_off)
         }
     } else {
         MWindowPropertyCache *pc;
-        if (!lockscreen_painted && (pc = findLockScreen())) {
+        if (device_state->touchScreenLock() == "locked" &&
+            !lockscreen_painted && (pc = findLockScreen())) {
             // lockscreen not painted yet: wait for painting or timeout
             if (!pc->isMapped())
                 // give it little time to map but not for ever

src/mdevicestate.cpp
@@ -38,6 +38,30 @@ void MDeviceState::mceDisplayStatusIndSignal(QString mode)
         }
     }
 }
+
+void MDeviceState::mceTouchScreenLockSignal(QString mode)
+{
+    if (tsmode_call) {
+        delete tsmode_call;
+        tsmode_call = 0;
+    }
+    touchScreenLockMode = mode;
+}
+
+void MDeviceState::gotTouchScreenLockMode(QDBusPendingCallWatcher *watcher)
+{
+    QDBusPendingReply<QString> reply = *watcher;
+    if (reply.isError()) {
+        qDebug() << __func__ << "getting touch screen lock mode failed:"
+                 << reply.error().message();
+        goto away;
+    }
+    touchScreenLockMode = reply;
+
+away:
+    delete tsmode_call;
+    tsmode_call = 0;
+}
 #endif
 
 void MDeviceState::callPropChanged()
@@ -71,13 +95,27 @@ MDeviceState::MDeviceState(QObject* parent)
     connect(top_prop, SIGNAL(valueChanged()), this, SLOT(topPropChanged()));
 
 #ifdef GLES2_VERSION
+    touchScreenLockMode = MCE_TK_UNLOCKED;
     systembus_conn = new QDBusConnection(QDBusConnection::systemBus());
+    if (!systembus_conn->isConnected())
+        qWarning("Failed to connect to the D-Bus system bus");
+
     systembus_conn->connect(MCE_SERVICE, MCE_SIGNAL_PATH, MCE_SIGNAL_IF,
                             MCE_DISPLAY_SIG, this,
                             SLOT(mceDisplayStatusIndSignal(QString)));
+    systembus_conn->connect(MCE_SERVICE, MCE_SIGNAL_PATH, MCE_SIGNAL_IF,
+                            MCE_TKLOCK_MODE_SIG, this,
+                            SLOT(mceTouchScreenLockSignal(QString)));
 
-    if (!systembus_conn->isConnected())
-        qWarning("Failed to connect to the D-Bus system bus");
+    // get the initial state of touch screen lock
+    tsmode_call = new QDBusPendingCallWatcher(
+        systembus_conn->asyncCall(
+            QDBusMessage::createMethodCall(MCE_SERVICE,
+                                           MCE_REQUEST_PATH,
+                                           MCE_REQUEST_IF,
+                                           MCE_TKLOCK_MODE_GET)), this);
+    connect(tsmode_call, SIGNAL(finished(QDBusPendingCallWatcher*)),
+            this, SLOT(gotTouchScreenLockMode(QDBusPendingCallWatcher*)));
 
     /* FIXME: Temporary workaround, current MCE does not seem to provide
      * get_display_status interface */

commit 5807f04ebbbad56075e0b9d050fca51dabdd2283

fix ut_lockscreen after 89fdfbbcfa42 broke it

src/mdevicestate.h
@@ -42,7 +42,8 @@ public:
     virtual bool displayOff() const { return display_off; }
     bool ongoingCall() const { return ongoing_call; }
     const QString &screenTopEdge() const { return screen_topedge; }
-    const QString &touchScreenLock() const { return touchScreenLockMode; }
+    virtual const QString &touchScreenLock() const
+    { return touchScreenLockMode; }
 
 signals:
 

tests/unit/ut_lockscreen/ut_lockscreen.cpp
@@ -69,6 +69,8 @@ public:
     fake_device_state() : fake_display_off(false) {}
     bool displayOff() const { return fake_display_off; }
     bool fake_display_off;
+    const QString &touchScreenLock() const { return fake_touchScreenLockMode; }
+    QString fake_touchScreenLockMode;
 };
 
 static fake_device_state *device_state;
@@ -84,6 +86,7 @@ void ut_Lockscreen::initTestCase()
     // create an altered MDeviceState
     device_state = new fake_device_state();
     cmgr->d->device_state = device_state;
+    device_state->fake_touchScreenLockMode = "locked";
 
     // create a fake desktop window
     fake_desktop_window *pc = new fake_desktop_window(1000);