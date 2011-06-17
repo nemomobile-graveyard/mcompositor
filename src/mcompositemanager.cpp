f025b7c383a77bbb460b964982a40b30382982df
Adam Endrodi <ext-adam.endrodi@nokia.com>
2011-06-17 21:28:36 +0300

    release EGL resources on restart
      * src/mcompositemanager.cpp (MCompositeManager::remoteControl):
        Turns out we have two contexts to terminate.

--- src/mcompositemanager.cpp
+++ src/mcompositemanager.cpp
@@ -4213,7 +4213,8 @@ void MCompositeManager::remoteControl(int cmdfd)
 
         delete d;
 #ifdef GLES2_VERSION
-        eglTerminate(QX11Info::display());
+        eglTerminate(eglGetDisplay(EGLNativeDisplayType(EGL_DEFAULT_DISPLAY)));
+        eglTerminate(eglGetDisplay(EGLNativeDisplayType(QX11Info::display())));
 #endif
         XFlush(QX11Info::display());
 
