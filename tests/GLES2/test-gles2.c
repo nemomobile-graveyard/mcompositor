0ac368e42780206ad41ec10834cb90fae19515f1
Kimmo Hämäläinen <kimmo.hamalainen@nokia.com>
2011-06-17 16:03:37 +0300

    Fixes: NB#266407 - Legal bug in compositor - fix few license headers

--- tests/GLES2/test-gles2.c
+++ tests/GLES2/test-gles2.c
@@ -1,9 +1,19 @@
-/* OpenGLES2 test program.
- *
- * Copyright 2010 (c) Nokia Corporation.
- *
- * Kimmo Hämäläinen <kimmo.hamalainen@nokia.com>
- */
+/***************************************************************************
+**
+** Copyright (C) 2010-2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (directui@nokia.com)
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at directui@nokia.com.
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
 
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
