nemomobile-graveyard/mcompositor

commit 55793caa905feb3f56af3682d661dc5bfe342eab

Fixes: NB#256211 - Device lock state:Home view is shown for a while (Before Security Lock screen appears) when new call to number is selected - MCompositeManagerPrivate::rootMessageEvent(): use skipStartupAnim to skip   activation animation when the window is under high-priority windows - MCallUiAnimation::windowClosed(): add missing setup for the animation - MCallUiAnimation::windowIconified(): new function to handle iconifying - MCallUiAnimation::endAnimation(): stack to bottom if window was iconified

src/mcompositemanager.cpp
@@ -2461,7 +2461,7 @@ void MCompositeManagerPrivate::rootMessageEvent(XClientMessageEvent *event)
                       // if it's not iconic, let the plugin decide
                       || (raise != topmost &&
                           !m_extensions.values(MapNotify).isEmpty()))) {
-                if (i->propertyCache()->noAnimations()) {
+                if (skipStartupAnim(i->propertyCache())) {
                     STACKING("positionWindow 0x%lx -> top", i->window());
                     positionWindow(i->window(), true);
                 } else {

src/mdynamicanimation.cpp
@@ -440,6 +440,7 @@ void MCallUiAnimation::windowClosed()
         return;
     
     setEnabled(true);
+    setupBehindAnimation();
     setupCallMode(false);
     targetWindow()->setVisible(true);
 
@@ -448,6 +449,7 @@ void MCallUiAnimation::windowClosed()
     cropper->installEffect(targetWindow());
     if ((behindTarget = targetWindow()->behind()) != NULL)
         cropper->installEffect(behindTarget);
+    cropper->setPortrait(targetWindow()->propertyCache()->orientationAngle() % 180);
     
     if (call_mode == MCallUiAnimation::IncomingCall)
         animationGroup()->setDirection(QAbstractAnimation::Backward);
@@ -459,6 +461,11 @@ void MCallUiAnimation::windowClosed()
         targetWindow()->behind()->setVisible(true);
     }
     start();
+}
+
+void MCallUiAnimation::windowIconified()
+{
+    windowClosed();
 }
 
 void MCallUiAnimation::setupBehindAnimation()
@@ -524,10 +531,13 @@ void MCallUiAnimation::endAnimation()
         behind->setUntransformed();
         behind->setPos(behind->propertyCache()->realGeometry().topLeft());
     }
-    // always stack the call-ui on top when finishing the animation 
-    if (targetWindow()->propertyCache()->windowState() != IconicState)
+    // stack the call-ui when finishing the animation 
+    if (targetWindow()->propertyCache()->windowState() == NormalState)
         m->positionWindow(targetWindow()->window(),
                           MCompositeManager::STACK_TOP);
+    else if (targetWindow()->propertyCache()->windowState() == IconicState)
+        m->positionWindow(targetWindow()->window(),
+                          MCompositeManager::STACK_BOTTOM);
 }
 
 #include "mdynamicanimation.moc"

src/mdynamicanimation.h
@@ -85,6 +85,7 @@ class MCallUiAnimation: public MDynamicAnimation
 
     virtual void windowShown(); 
     virtual void windowClosed();
+    void windowIconified();
     void setupBehindAnimation();
     void setupCallMode(bool showWindow = true);
         

commit 87bce7841dafed66f9ea21c7a2539f497b88a5da

fix a regression caused by 55793caa905feb3f - don't skip activation animation for iconic windows

src/mcompositemanager.cpp
@@ -2179,14 +2179,16 @@ void MCompositeManagerPrivate::stackingTimeout()
 }
 
 // check if there is a categorically higher mapped window than pc
-bool MCompositeManagerPrivate::skipStartupAnim(MWindowPropertyCache *pc)
+// iconic_is_ok: true if animation will raise the window above desktop
+bool MCompositeManagerPrivate::skipStartupAnim(MWindowPropertyCache *pc,
+                                               bool iconic_is_ok)
 {
     if (pc->noAnimations())
         return true;
     // Ignore initial_state == IconicState if the client stacked the window
     // somewhere, then only skip if it's below the desktop (which is still
     // not correct but better than nothing).
-    if (stack[DESKTOP_LAYER] && !pc->stackedUnmapped()) {
+    if (!iconic_is_ok && stack[DESKTOP_LAYER] && !pc->stackedUnmapped()) {
         const XWMHints &h = pc->getWMHints();
         if ((h.flags & StateHint) && h.initial_state == IconicState)
             return true;
@@ -2201,11 +2203,14 @@ bool MCompositeManagerPrivate::skipStartupAnim(MWindowPropertyCache *pc)
         Window w = stacking_list.at(i);
         if (w == pc->winId())
             above = true;
-        if (w && w == stack[DESKTOP_LAYER])
+        if (w && w == stack[DESKTOP_LAYER]) {
+            if (iconic_is_ok)
+                return false;
             // skip the animation if the window is below the desktop,
             // but not if the window hasn't been stacked, because then
             // the animation *would* raise it
             return !above && pc->stackedUnmapped();
+        }
         if (!(tmp = prop_caches.value(w, 0)) || tmp->isInputOnly()
             || tmp == pc || !tmp->isMapped() || tmp->isDecorator())
             continue;
@@ -2463,7 +2468,7 @@ void MCompositeManagerPrivate::rootMessageEvent(XClientMessageEvent *event)
                       // if it's not iconic, let the plugin decide
                       || (raise != topmost &&
                           !m_extensions.values(MapNotify).isEmpty()))) {
-                if (skipStartupAnim(i->propertyCache())) {
+                if (skipStartupAnim(i->propertyCache(), true)) {
                     STACKING("positionWindow 0x%lx -> top", i->window());
                     positionWindow(i->window(), true);
                 } else {

src/mcompositemanager_p.h
@@ -128,7 +128,7 @@ public:
     bool processX11EventFilters(XEvent *event, bool after);
     void removeWindow(Window w);
     bool needDecoration(MWindowPropertyCache *pc);
-    bool skipStartupAnim(MWindowPropertyCache *pc);
+    bool skipStartupAnim(MWindowPropertyCache *pc, bool iconic_is_ok = false);
     MCompositeWindow *getHighestDecorated(int *index = 0);
     void setWindowStateForTransients(MWindowPropertyCache *pc, int state,
                                      int level = 0);