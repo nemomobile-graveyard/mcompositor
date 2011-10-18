14583972d96ff0f22e40080270a76763eae3b502
Armin Berres <armin.berres@basyskom.de>
2011-10-18 11:38:06 +0200

    Fixes: NB#280638 - splash screen flashes when opening documents
    Details:
    Increase the damage timeout for splashed apps to make sure we fade to
    the actual content.
    It makes sense to wait longer here as there is not need for instant
    visual feedback - the splash screen is the visual feedback.

--- src/mcompositewindow.cpp
+++ src/mcompositewindow.cpp
@@ -263,7 +263,13 @@ void MCompositeWindow::waitForPainting()
     waiting_for_damage = mc->configInt("damages-for-starting-anim");
     resize_expected = false;
     painted_after_mapping = false;
-    damage_timer->setInterval(mc->configInt("damage-timeout-ms"));
+    // Give splashed apps a bit more time to draw the actual content
+    if (mc->d->splash
+            && mc->d->splash->propertyCache()->pid() == propertyCache()->pid()) {
+        damage_timer->setInterval(mc->configInt("splashed-damage-timeout-ms"));
+    } else {
+        damage_timer->setInterval(mc->configInt("damage-timeout-ms"));
+    }
     damage_timer->start();
 }
 
