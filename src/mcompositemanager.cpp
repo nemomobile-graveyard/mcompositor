e448eb56d0dfd3c7af75dd9c6af60dd5885d6b13
Armin Berres <armin.berres@basyskom.de>
2011-10-19 17:36:56 +0200

    Changes: move MSGrabber::grabLater() into the cpp file to allow setting a breakpoint there
    Details:
    Having it in the header does not make sense anyway. This part is
    everything but not performance critical.

--- src/mcompositemanager.cpp
+++ src/mcompositemanager.cpp
@@ -4789,3 +4789,8 @@ void MSGrabber::reinforce()
     Q_ASSERT(has_grab && mercytimer.isActive());
     mercytimer.start();
 }
+
+void MSGrabber::grabLater(bool setting)
+{
+    needs_grab = setting;
+}
