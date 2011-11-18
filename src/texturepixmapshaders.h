7ee7a1be47b34bff7ec11dfd2ad13e1602d144c4
Abdiel Janulgue <abdiel.janulgue@nokia.com>
2011-11-18 15:53:16 +0200

    Changes: Split opacity shader from opaque shader. Remove unused blur shader

--- src/texturepixmapshaders.h
+++ src/texturepixmapshaders.h
@@ -34,6 +34,14 @@ static const char* TexpVertShaderSource = "\
 static const char* TexpFragShaderSource = "\
     varying mediump vec2 fragTexCoord;\
     uniform sampler2D texture;\
+    void main(void) \
+    {\
+            gl_FragColor = texture2D(texture, fragTexCoord); \
+    }";
+
+static const char* TexpOpacityFragShaderSource = "\
+    varying mediump vec2 fragTexCoord;\
+    uniform sampler2D texture;\
     uniform lowp float opacity;\n\
     void main(void) \
     {\
@@ -48,60 +56,4 @@ static const char* TexpCustomShaderSource = "\
     {\n\
             gl_FragColor = customShader(texture, fragTexCoord) * opacity; \n\
     }\n\n";
-
-
-#if 0
-static const char *AlphaTestFragShaderSource = "\
-    varying lowp vec2 fragTexCoord;\
-    uniform sampler2D texture0;\
-    void main(void) \
-    {\
-        vec4 baseColor = texture2D(texture0, fragTexCoord);\
-        lowp float v = baseColor.r + baseColor.g + baseColor.b;   \
-        if(v == 0.0)\
-            discard;\
-        else\
-            gl_FragColor = baseColor;\
-    }";
-#endif
-
-static const char *blurshader = "\
-varying lowp vec2 fragTexCoord;\
-uniform sampler2D texture0;\
-uniform mediump float blurstep;\
-void main(void)\
-{\
-mediump vec4 sample0,\
-sample1,\
-sample2,\
-sample3;\
-mediump float step = blurstep / 100.0;\
-mediump vec2 upperleft = fragTexCoord.xy - vec2(step);\
-mediump vec2 lowerright = fragTexCoord.xy + vec2(step);\
-mediump vec2 upperright = fragTexCoord.xy + vec2(step, -step);\
-mediump vec2 lowerleft = fragTexCoord.xy + vec2(-step, step);\
-sample0 = texture2D(texture0, upperleft); \
-sample1 = texture2D(texture0, lowerright); \
-sample2 = texture2D(texture0, upperright); \
-sample3 = texture2D(texture0, lowerleft); \
-gl_FragColor = ((sample0 + sample1 + sample2 + sample3) / 4.0) * 0.5;\
-}";
-
-
-/* static const char* AlphaTestFragShaderSource = "\ */
-/*     precision lowp float;\ */
-/*     varying lowp vec2 fragTexCoord;\ */
-/*     uniform sampler2D texture0;\ */
-/*     void main(void) \ */
-/*     {\ */
-/*               vec4 baseColor = texture2D(texture0, fragTexCoord);   \ */
-/*                if(baseColor.r == 0.0 &&\ */
-/*                   baseColor.g == 0.0 &&\ */
-/*                   baseColor.b == 0.0)\ */
-/*                   discard;\ */
-/*                else\ */
-/*                   gl_FragColor = baseColor;\ */
-/*     }"; */
-
-
 #endif
